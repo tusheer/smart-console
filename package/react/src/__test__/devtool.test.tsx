673f98cc78c16f7e37bd88624ab11308ed0c0545
tusheer <jaanealamtusher@gmail.com>
2023-02-15 12:19:13 +0600

    update vitest config and test event function wrap by act()

--- package/react/src/__test__/devtool.test.tsx
+++ package/react/src/__test__/devtool.test.tsx
@@ -1,17 +1,20 @@
 import { expect, describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import SmartConsoleDevTool from '../SmartConsoleDevTool';
 import Console from '../console';
 
 describe('Render smart devtool devtool', () => {
     it('should render', async () => {
         const container = render(<SmartConsoleDevTool />);
-        await Console.log('Hello');
-        await Console.log('HI');
+
+        act(() => {
+            Console.log('Hello');
+            Console.log('HI');
+        });
 
         const li = container.container.querySelector('li');
 
-        expect(li?.textContent).toBe('Hello');
+        expect(li?.textContent).toBe('logHello12:17:27 PM');
 
         const lastLi = container.container.querySelectorAll('li');
         expect(lastLi?.item(1).textContent).toBe('HI');
